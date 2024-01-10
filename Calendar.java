5ce502bc7f485216650daf09cde29f338ef6d749
AbedalsalamAkel <154346157+AbedalsalamAkel@users.noreply.github.com>
2024-01-10 19:20:49 +0200

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,70 +1,103 @@
 
-public class Calendar {
-    static int day = 1;
-    static int month = 1;
-    static int year;
-    static int daysInMonth = 31; // January has 31 days
+/** 
+ * Prints the calendars of all the years in the 20th century.
+ */
+public class Calendar {	
+    // Starting the calendar on 1/1/1900
+	static int dayOfMonth = 1;   
+	static int month = 1;
+	static int year = 1900;
+	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
+	static int nDaysInMonth = 31; // Number of days in January
+	
+	/** 
+	 * Prints the calendars of all the years in the 20th century. Also prints the  
+	 * number of Sundays that occured on the first day of the month during this period.
+	 */
+	public static void main(String args[]) 
+    {
+        int checkyear= Integer.parseInt(args [0]);
+        int sundayCounter=0;
+		while (year<checkyear)
+         {
+            advance();
+         }
+         while	(year==checkyear)
+         {
+           System.out.print(dayOfMonth+"/"+month+"/"+year);
+            if(dayOfWeek==1)
+			{
+				System.out.print(" Sunday");
+			}
+			System.out.println();
+			advance();
 
-    public static void main(String[] args) {
-        // Check if a year has been provided
-        if (args.length < 1) {
-            System.out.println("Please provide a year as an argument.");
-            return;
-        }
-
-        // Parse the provided year
-        year = Integer.parseInt(args[0]);
-        int endYear = year + 1; // The year after the provided year
-
-        // Loop through the entire year provided
-        while (year < endYear) {
-            // Print the current date
-            System.out.println(day + "/" + month + "/" + year);
-
-            // Move to the next day
-            day++;
-            if (day > daysInMonth) {
-                day = 1; // Reset to the first day
-                month++; // Move to the next month
-
-                if (month > 12) {
-                    month = 1; // Reset to January
-                    year++; // Increment the year
-                }
-
-                // Update the number of days in the new month
-                daysInMonth = getDaysInMonth(month, year);
-            }
         }
     }
 
-    // Method to determine if a year is a leap year
-    private static boolean isLeapYear(int year) {
-        // Leap year if divisible by 4 but not by 100, or divisible by 400
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	 
+	
+	 // Advances the date (day, month, year) and the day-of-the-week.
+	 // If the month changes, sets the number of days in this month.
+	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
+	 private static void advance() 
+	 {
+		if(month==12 && dayOfMonth==31)// if you made it to the end of the year start a new one
+		{
+			year++;
+			month=1;
+			dayOfMonth=1;
+		}
+		else
+		{
 
-    // Method to get the number of days in a month
-    private static int getDaysInMonth(int month, int year) {
-        // February has 28 or 29 days
-        if (month == 2) {
-            if (isLeapYear(year)) {
-                return 29;
-            } else {
-                return 28;
-            }
-        }
-        // April, June, September, November have 30 days
-        else if (month == 4 || month == 6 || month == 9 || month == 11) {
-            return 30;
-        }
-        // All other months have 31 days
-        else {
-            return 31;
-        }
-    }
+			if (dayOfMonth==nDaysInMonth)//if you made it to the end of the month start a new one, else advance
+			{
+				month++;
+				if (month==13)
+					month=1;
+				nDaysInMonth=nDaysInMonth(month, year);
+				dayOfMonth=1;
+
+			}
+			else
+				dayOfMonth++;
+		}
+		if(dayOfWeek==7)//if you made it to the end of the week, start over
+			dayOfWeek=1;
+		else
+			dayOfWeek++;
+
+	} 
+		 
+    // Returns true if the given year is a leap year, false otherwise.
+	private static boolean isLeapYear(int year) {
+		boolean isLeapYear=false;
+		isLeapYear = ((year % 400) == 0);
+		isLeapYear = isLeapYear || (((year % 4) == 0) && ((year % 100) != 0));
+	 	return isLeapYear;		
+	}
+	 
+	private static int nDaysInMonth(int month, int year)
+	 {
+		int days = 31;
+		switch (month)
+		{
+			case 4: days= 30 ;
+			break;
+			case 6: days= 30 ;
+			break;
+			case 9: days= 30 ;
+			break;
+			case 11: days= 30 ;
+			break;
+			case 2: 
+				if (isLeapYear(year))
+					days=29;
+				else
+					days=28;
+				break;
+			}
+		return days;		
+	}
 }
